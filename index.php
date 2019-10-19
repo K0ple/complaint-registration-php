6eb4f69330b0b5131fc59f86e3918ca15d6e3375
NICKY <nikhil6raga@gmail.com>
2019-10-19 22:06:33 +0530

    commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<?php include "bar.php"; ?>
+<head>
+    <title>HTML</title>
+    <link rel="import" href="NAVIGATIONBAR.html" >
+<link rel="stylesheet" type="text/css" href="maincss.css">
+
+</head>
+
+<body>
+    <h1>PUBLIC GRIEVANCE SYSTEM</h1>
+    <div class="top">
+        <div class="main">
+            <form action="" method="POST">
+            <a href="add_complaint.php">
+                <div class="left" name="ROAD ACCIDENT">ROAD ACCIDENT</div></a>
+            </form>
+                <a href="/corruption">
+                <div class="right" name="CORRUPTION">CORRUPTION</div></a>
+            
+        </div>
+        <div class="main1">
+            <a href="/garbage">
+                <div class="left1" name="GARBAGE PROBLEMS">GARBAGE PROBLEMS</div></a>
+            <a href="/waterleakage">
+                <div class="right1" name="WATER LEAKAGE">WATER LEAKAGE</div></a>
+        </div>
+        <div class="main2">
+            <a href="/robbery">
+                <div class="left2" name="ROBBERY">ROBBERY</div></a>
+            <a href="/cybercrime">
+                <div class="right2" name="CYBER CRIME">CYBER CRIME</div></a>
+            </div>
+        <div class="main3">
+            <a href="/sexualharassment">
+                <div class="left3" name="SEXUAL HARASSMENT">SEXUAL HARASSMENT</div></a>
+            <a href="/domesticviolence">
+                <div class="right3" name="DOMESTIC VIOLENCE">DOMESTIC VIOLENCE</div></a>
+        </div>
+    </div>
+</body>
+</html>
