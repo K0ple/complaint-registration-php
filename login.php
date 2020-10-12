b8936376b99d6b7a7d005d55cc8f6be16ae129fd
Satyaki Suman <satyaki.suman@gmail.com>
2020-10-12 20:38:00 +0530

    login and signup

--- login.php
+++ login.php
@@ -8,6 +8,43 @@
 </head>
 
 <body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <!-- Image and text -->
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="img/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""
+          loading="lazy" />
+        Complaint Registry
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="main.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="complaint.php">Complaint</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="signup.php">Sign Up</a>
+            </li>
+
+        </div>
+      </div>
+    </div>
+  </nav>
     <div class="feature-wrapper pt-5 pb-5 mt-5 mt-lg-5">
         <center><h1> Complaint Registration Portal</h1></center>
     </div>
--- signup.php
+++ signup.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <head>
-    <title>SignUp</title>
-</div>
-<link rel="stylesheet" type="text/css" href="signupcss.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="screen" />
 </head>
-<?php 
-include('bar.php');
-?>
+
 <body>
 <body>
-<div class="messages">
+<!-- <div class="messages">
     <div class="log"> 
         <img src="https://www.paceind.com/wp-content/uploads/2016/09/display-14.png">
         <form method="post" action="<?php $_SERVER["PHP_SELF"] ?>" enctype="multipart/form-data" >
@@ -31,6 +30,85 @@ include('bar.php');
         </div>
             <input class="sub" type="submit" value="Sign Up">
         </form>
+    </div> -->
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <!-- Image and text -->
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="img/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""
+          loading="lazy" />
+        Complaint Registry
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="main.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="complaint.php">Complaint</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="signup.php">Sign Up</a>
+            </li>
+
+        </div>
+      </div>
+    </div>
+  </nav>
+    <div class="feature-wrapper pt-5 pb-5 mt-5 mt-lg-5">
+        <center><h1> Complaint Registration Portal</h1></center>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-6 m-auto">
+            <div class="card card-body mt-5">
+                <h1 class="text-center mb-3">
+                    <i class="fas fa-user-plus"></i> Register
+                </h1>
+                <form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="name" id="name" name="name" class="form-control" placeholder="Enter Name"
+                            value="" />
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Enter Email"
+                            value="" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" class="form-control"
+                            placeholder="Create Password"
+                            value="" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password2">Confirm Password</label>
+                        <input type="password" id="password2" name="password2" class="form-control"
+                            placeholder="Confirm Password"
+                            value="" />
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">
+                        Register
+                    </button>
+                </form>
+                <p class="lead mt-4">Have An Account? <a href="login.php">Login</a></p>
+            </div>
+        </div>
     </div>
 
     <?php
